d739ba6316a4a7a47942745b82d8d9c06b9b7df2
Christian Berendt <berendt@betacloud-solutions.de>
2017-12-07 13:47:55 +0100

    quotamanager: overall improvements

--- quotamanager/quotamanager.py
+++ quotamanager/quotamanager.py
@@ -4,15 +4,25 @@ import os_client_config
 import shade
 import yaml
 
-with open("quotaclasses.yml", "r") as fp:
-    quotaclasses = yaml.load(fp)
+CLOUDNAME = 'service'
 
 #shade.simple_logging(debug=True, http_debug=True)
 
-cloud = shade.operator_cloud(cloud='service')
-keystone = os_client_config.make_client('identity', cloud='service')
+def set_quotaclass(project, quotaclass="default"):
+    keystone = os_client_config.make_client('identity', cloud=CLOUDNAME)
+    keystone.projects.update(project.id, **{"quotaclass": quotaclass})
 
-for project in [p for p in cloud.search_projects() if not p.is_domain]:
+def update_quota(project, cloud):
+    print "update network quota for %s" % project.name
+    cloud.set_network_quotas(project.id, **quotaclasses[project.quotaclass]["network"])
+
+    print "update compute quota for %s" % project.name
+    cloud.set_compute_quotas(project.id, **quotaclasses[project.quotaclass]["compute"])
+
+    print "update volume quota for %s" % project.name
+    cloud.set_volume_quotas(project.id, **quotaclasses[project.quotaclass]["volume"])
+
+def check_quota(project, cloud):
 
     if "quotaclass" in project and project.name not in ["admin", "service"]:
 
@@ -22,30 +32,33 @@ for project in [p for p in cloud.search_projects() if not p.is_domain]:
             if quotaclasses[project.quotaclass]["network"][key] != quotanetwork[key]:
                 quotaupdate = True
                 print "%s [ %s ] %d != %d" % (project.name, key, quotaclasses[project.quotaclass]["network"][key], quotanetwork[key])
-        if quotaupdate:
-            print "update network quota for %s" % project.name
-            cloud.set_network_quotas(project.id, **quotaclasses[project.quotaclass]["network"])
 
         quotacompute = cloud.get_compute_quotas(project.id)
-        quotaupdate = False
         for key in quotaclasses[project.quotaclass]["compute"]:
             if quotaclasses[project.quotaclass]["compute"][key] != quotacompute[key]:
                 quotaupdate = True
                 print "%s [ %s ] %d != %d" % (project.name, key, quotaclasses[project.quotaclass]["compute"][key], quotacompute[key])
-        if quotaupdate:
-            print "update compute quota for %s" % project.name
-            cloud.set_compute_quotas(project.id, **quotaclasses[project.quotaclass]["compute"])
 
         quotavolume = cloud.get_volume_quotas(project.id)
-        quotaupdate = False
         for key in quotaclasses[project.quotaclass]["volume"]:
             if quotaclasses[project.quotaclass]["volume"][key] != quotavolume[key]:
                 quotaupdate = True
                 print "%s [ %s ] %d != %d" % (project.name, key, quotaclasses[project.quotaclass]["volume"][key], quotavolume[key])
+
         if quotaupdate:
-            print "update volume quota for %s" % project.name
-            cloud.set_volume_quotas(project.id, **quotaclasses[project.quotaclass]["volume"])
+            update_quota(project, cloud)
 
     elif "quotaclass" not in project and project.name not in ["admin", "service"]:
         print "quotaclass not set for project %s, set quotaclass to default" % project.name
-        keystone.projects.update(project.id, **{"quotaclass": "default"})
+        set_quotaclass(project, "default")
+        project = cloud.get_project(project.id)
+        update_quota(project, cloud)
+
+
+with open("quotaclasses.yml", "r") as fp:
+    quotaclasses = yaml.load(fp)
+
+cloud = shade.operator_cloud(cloud=CLOUDNAME)
+
+for project in [p for p in cloud.search_projects() if not p.is_domain]:
+    check_quota(project, cloud)
