7396bde00dd7f2deece53002155c901c0cd01bf7
Christian Berendt <berendt@betacloud-solutions.de>
2017-12-12 19:51:27 +0100

    Add run script

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [[ $# -lt 1 ]]; then
+    echo usage: $0 TOOL [...]
+    exit 1
+fi
+
+tool=$1
+shift
+
+if [[ ! -e $tool ]]; then
+    echo tool $tool not available
+    exit 1
+fi
+
+BASE_DIRECTORY=${BASE_DIRECTORY:-/opt/tools}
+
+command -v pip >/dev/null 2>&1 || { echo >&2 "pip not installed"; exit 1; }
+command -v virtualenv >/dev/null 2>&1 || { echo >&2 "virtualenv not installed"; exit 1; }
+
+pushd $BASE_DIRECTORY
+
+if [[ ! -e .venv-$tool ]]; then
+    virtualenv .venv-$tool
+    source .venv-$tool/bin/activate
+    pip install -r $tool/requirements.txt
+else
+    source .venv-$tool/bin/activate
+fi
+
+python $tool/$tool.py $*
+
+popd
